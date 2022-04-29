a7aaaf9ad875135f802f721016268560c00958e6
Chris Lubomanski <lubomanski@gmail.com>
2022-04-29 21:29:17 +0200

    Update skeletyl

--- keyboards/bastardkb/skeletyl/keymaps/default/keymap.c
+++ keyboards/bastardkb/skeletyl/keymaps/default/keymap.c
@@ -35,7 +35,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [2] = LAYOUT_split_3x5_3(
     KC_EXLM,   KC_AT, KC_HASH,  KC_DLR,    KC_PERC,                  KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN,
-    XXXXXXX, KC_GRV, KC_QUOTE,  KC_LCBR,   KC_LBRC,                  KC_RBRC,  KC_RCBR, KC_DQUO, KC_RBRC, KC_BSLS,
+    XXXXXXX, KC_GRV, KC_QUOTE,  KC_LCBR,   KC_LBRC,                  KC_RBRC,  KC_RCBR, KC_DQUO, KC_TILD, KC_BSLS,
     XXXXXXX, KC_TILD, XXXXXXX,  KC_EQL,    KC_MINUS,                 KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE,
                       KC_LGUI,   KC_TRNS,  KC_ENT,                   KC_BSPC,   MO(4), KC_DEL
   ),
@@ -44,7 +44,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                        KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
     KC_ESC, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                     KC_LEFT, KC_DOWN,   KC_UP,KC_RIGHT, XXXXXXX,
     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_TAB,                      KC_NO, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-                       KC_LGUI,   MO(4),  KC_DEL,                    KC_SPC,   KC_TRNS, KC_RALT
+                       KC_DEL,   MO(4),  KC_LGUI,                    KC_SPC,   KC_TRNS, KC_RALT
   ),
 
   [4] = LAYOUT_split_3x5_3(
